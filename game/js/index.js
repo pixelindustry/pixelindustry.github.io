093109ec93b98f7fa19aca3501f2a398a74cb555
VAVILBISK | shmeleon.tk <63008469+shmeleon@users.noreply.github.com>
2021-11-14 01:42:09 +0300

    1`

---
+++ game/js/index.js
@@ -0,0 +1,21 @@
+/*
+JQUERY PLUGIN FLAPPY BIRD FOR YOUR WEB SITE!! 1.0BETA
+FULL VERSION WITH RATE SYSTEM: http://troevkube.ru/flappy/
+
+You can buy this plugin for your site here: http://bit.ly/MEvzN0
+
+What is this?
+Flappy bird is an easy-to-embed single-line JavaScript plugin that will add this epic game to your webpage.
+
+all question  please send me to mail: grommy03@gmail.com
+Facebook: https://www.facebook.com/troshkinpavel
+Vkontakte: https://vk.com/troshkin_pavel
+*/
+$(document).ready(function() {
+		// CALL PLUGIN WITH BEGIN PARAMETERS
+		$('.container').FlappyBird({
+			// total : 8,
+   	// step : 300,
+   	// innerColumn: 170
+		});
+	});
